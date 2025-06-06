c727f6c4d9bfa7658d180836da96309d034e727a
rzofchak8 <r.zofchak8@gmail.com>
2025-06-05 22:08:21 -0400

    rename to singular to conform to database tables

--- api/__init__.py
+++ api/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 # from db import get_db  # Import shared get_db function
 
 api_bp = Blueprint("api", __name__, url_prefix="/api")
-from . import categories, houses, photos, rooms, tasks, users
+from . import category, house, photo, room, task, user
 
 # Make get_db available to all API modules
 # __all__ = ['api_bp', 'get_db']
